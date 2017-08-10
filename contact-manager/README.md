f3b88fadf2f53705d729e9622d7d31369855e68c
Taylor Brazelton <ryanb58@live.com>
2017-08-09 23:36:36 -0400

    Got contact manager working a tad.

---
+++ contact-manager/README.md
@@ -0,0 +1,5 @@
+### Tutorial:
+
+http://aurelia.io/hub.html#/doc/article/aurelia/framework/latest/contact-manager-tutorial/1
+
+
--- contact-manager/src/app.js
+++ contact-manager/src/app.js
@@ -1,5 +1,13 @@
+import {inject} from 'aurelia-framework';
+import {WebAPI} from './web-api';
+
+@inject(WebAPI)
 export class App {
-  configureRouter(config, router){
+  constructor(api) {
+    this.api = api;
+  }
+
+  configureRouter(config, router) {
     config.title = 'Contacts';
     config.map([
       { route: '',              moduleId: 'no-selection',   title: 'Select'},
